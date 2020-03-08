6c1f44a7f6302cc973cb685e71e36a5419a1558a
Rubens Batista <batistarubinho@gmail.com>
2020-03-07 23:51:02 -0300

    Criado um arquivo com os tamanhos de fontes e cores bases da pagina

---
+++ src/styles/settings/index.js
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: "#404040",
+  secondary: "#009CA3",
+  third: "#2B0D61",
+  fourth: "#B5ABD4"
+};
+
+export const font = {
+  verySmall: ".5rem",
+  small: "1.3rem",
+  medium: "1rem",
+  big: "1.5rem"
+};
