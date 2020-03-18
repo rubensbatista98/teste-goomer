d284d9924fd4a30700dcde040008b405183f12a3
Rubens Batista <batistarubinho@gmail.com>
2020-03-18 01:21:43 -0300

    Refatorada a pagina RestaurantMenu, para fazer tratamento de erro e fazer filtragem dos pratos do cardapio

--- src/pages/RestaurantMenu/index.js
+++ src/pages/RestaurantMenu/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useCallback, useContext } from "react";
 import { useParams } from "react-router-dom";
 
 import FormSearch from "../../components/FormSearch";
@@ -11,7 +11,9 @@ import {
   Aside,
   Image,
   Title,
-  Schedule
+  Schedule,
+  Error,
+  NotFound
 } from "./styles";
 
 import api from "../../services/api";
@@ -19,9 +21,35 @@ import api from "../../services/api";
 const RestaurantMenu = () => {
   const [restaurant, setRestaurant] = useState({});
   const [menu, setMenu] = useState([]);
+  const [filteredDishes, setFilteredDishes] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
+
   const [restaurants] = useContext(RestaurantsContext);
 
   const { id } = useParams();
+
+  const filterDishsByName = useCallback(
+    name => {
+      if (name.length === 0) {
+        setFilteredDishes([...menu]);
+        return null;
+      }
+
+      const filtered = menu.filter(dish =>
+        dish.name.toLowerCase().includes(name.toLowerCase().trim())
+      );
+
+      if (filtered.length === 0) {
+        setNotFound(true);
+        return null;
+      }
+
+      setFilteredDishes(filtered);
+      setNotFound(false);
+    },
+    [menu]
+  );
 
   useEffect(() => {
     const restaurant = restaurants.find(
@@ -33,13 +61,23 @@ const RestaurantMenu = () => {
 
   useEffect(() => {
     async function loadMenu() {
-      const response = await api(`restaurants/${id}/menu`);
+      try {
+        setError(false);
 
-      setMenu(response);
+        const response = await api(`restaurants/${id}/menu`);
+
+        setMenu(response);
+        setFilteredDishes([...response]);
+      } catch (e) {
+        setError(true);
+      }
     }
 
     loadMenu();
   }, [id]);
+
+  if (error) return <Error>Desculpe, mas ocorreu um erro inesperado.</Error>;
+
   return (
     <Wrapper>
       <Header>
@@ -63,9 +101,13 @@ const RestaurantMenu = () => {
       </Header>
 
       <Section>
-        <FormSearch bgColor="#E6E6E6" />
+        <FormSearch bgColor="#E6E6E6" filterByValue={filterDishsByName} />
 
-        <AccordionMenu menu={menu} />
+        {notFound ? (
+          <NotFound>O prato buscado não esta disponível no cardápio.</NotFound>
+        ) : (
+          <AccordionMenu menu={filteredDishes} />
+        )}
       </Section>
 
       <Aside />
--- src/pages/RestaurantMenu/styles.js
+++ src/pages/RestaurantMenu/styles.js
@@ -74,3 +74,15 @@ export const Aside = styled.aside`
     max-height: 200px;
   }
 `;
+
+export const Error = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: center;
+  margin: 40px 10px;
+`;
+
+export const NotFound = styled(Error)`
+  margin: 50px 20px;
+`;
