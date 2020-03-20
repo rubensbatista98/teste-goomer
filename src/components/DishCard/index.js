e52e0b643a3bb11d731092155c6fc179c943134c
Rubens Batista <batistarubinho@gmail.com>
2020-03-20 01:31:26 -0300

    Refatorado o DishCard, para usar a funcao utilitaria, para poder verificar se a promocao e valida pro horario

--- src/components/DishCard/index.js
+++ src/components/DishCard/index.js
@@ -11,6 +11,8 @@ import {
   Promo
 } from "./styles";
 
+import getStatusByHours from "../../utils/getStatusByHours";
+
 const DishCard = ({ dish }) => {
   const [promoIsActive, setPromoIsActive] = useState(false);
   const [promo, setPromo] = useState({});
@@ -27,99 +29,13 @@ const DishCard = ({ dish }) => {
 
     const { sales } = dish;
 
-    const MILLISECONDS_IN_A_DAY = 86400000;
-    const timeNow = new Date(2020, 2, 17, 20, 30, 0);
-    const status = [];
-
-    const getDate = (date, hour, minutes) =>
-      new Date(
-        timeNow.getFullYear(),
-        timeNow.getMonth(),
-        date,
-        hour,
-        minutes,
-        0
-      );
-
     const salesStatus = sales.reduce((salesStatus, sale) => {
-      const salesValidToday = sale.hours.filter(hour => {
-        return hour.days.indexOf(timeNow.getDay() + 1) > -1;
-      });
+      const saleStatus = getStatusByHours(sale.hours);
 
-      if (salesValidToday.length === 0) {
-        salesStatus.push({
-          isActiveNow: false,
-          timeToChange: MILLISECONDS_IN_A_DAY
-        });
-      }
-
-      if (salesValidToday.length > 0) {
-        salesValidToday.forEach(hour => {
-          const { from, to, days } = hour;
-
-          const startingHour = from.split(":");
-          const endingHour = to.split(":");
-
-          const startingTime = getDate(
-            timeNow.getDate(),
-            startingHour[0],
-            startingHour[1]
-          );
-
-          const endingTime = getDate(
-            startingHour[0] > endingHour[0]
-              ? timeNow.getDate() + 1
-              : timeNow.getDate(),
-            endingHour[0],
-            endingHour[1]
-          );
-
-          const isActiveNow =
-            timeNow.getTime() >= startingTime.getTime() &&
-            timeNow.getTime() < endingTime.getTime() &&
-            days.indexOf(startingTime.getDay() + 1) > -1;
-
-          let timeToChange = null;
-
-          if (isActiveNow) {
-            timeToChange = endingTime.getTime() - timeNow.getTime();
-          }
-
-          if (!isActiveNow) {
-            const nowIsBeforeStarting =
-              timeNow.getTime() < startingTime.getTime();
-
-            const nowIsAfterEnding = timeNow.getTime() >= endingTime.getTime();
-
-            if (nowIsBeforeStarting) {
-              timeToChange = startingTime.getTime() - timeNow.getTime();
-            }
-
-            if (nowIsAfterEnding) {
-              const tomorrowTime = getDate(
-                timeNow.getDate() + 1,
-                startingHour[0],
-                startingHour[1]
-              );
-
-              timeToChange = tomorrowTime.getTime() - timeNow.getTime();
-            }
-          }
-
-          status.push({ isActiveNow, timeToChange });
-        });
-
-        const activeStatus = status.find(hour => hour.isActiveNow);
-
-        if (activeStatus) {
-          salesStatus.push({ ...activeStatus, sale });
-        } else {
-          const orderedStatusByTime = [...status].sort(
-            (a, b) => a.timeToChange - b.timeToChange
-          );
-
-          salesStatus.push(orderedStatusByTime[0]);
-        }
+      if (saleStatus.isActiveNow) {
+        salesStatus.push({ ...saleStatus, sale });
+      } else {
+        salesStatus.push(saleStatus);
       }
 
       return salesStatus;
