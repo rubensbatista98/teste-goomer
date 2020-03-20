544dbcfdecdaf414b7ca71ac7e024fcb7a9267cb
Rubens Batista <batistarubinho@gmail.com>
2020-03-20 01:29:20 -0300

    Refatorado o RestaurantCard, para utilizar a funcao utilitaria, para verificar se o restaurante esta aberto

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -2,95 +2,26 @@ import React, { useEffect, useCallback, useState } from "react";
 
 import { LinkGoomer, Card, Thumb, Header, Status } from "./styles";
 
+import getStatusByHours from "../../utils/getStatusByHours";
+
 const RestaurantCard = ({ restaurant }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const getIfIsOpen = useCallback(() => {
-    if (!restaurant?.hours) return { isOpenNow: false, timeToChange: null };
+    if (!restaurant?.hours) return { isActiveNow: false, timeToChange: null };
 
-    const timeNow = new Date();
-    const hoursStatus = [];
-
-    const getDate = (date, hour, minutes) =>
-      new Date(
-        timeNow.getFullYear(),
-        timeNow.getMonth(),
-        date,
-        hour,
-        minutes,
-        0
-      );
-
-    const hoursValidToday = restaurant.hours.filter(
-      hour => hour.days.indexOf(timeNow.getDay() + 1) > -1
-    );
-
-    hoursValidToday.forEach(hour => {
-      const { from, to, days } = hour;
-
-      const openingHour = from.split(":");
-      const closingHour = to.split(":");
-
-      const openingTime = getDate(
-        timeNow.getDate(),
-        openingHour[0],
-        openingHour[1]
-      );
-
-      const closingTime = getDate(
-        openingHour[0] > closingHour[0]
-          ? timeNow.getDate() + 1
-          : timeNow.getDate(),
-        closingHour[0],
-        closingHour[1]
-      );
-
-      const isOpenNow =
-        timeNow.getTime() >= openingTime.getTime() &&
-        timeNow.getTime() < closingTime.getTime() &&
-        days.indexOf(openingTime.getDay() + 1) > -1;
-
-      let timeToChange = null;
-
-      if (isOpenNow) timeToChange = closingTime.getTime() - timeNow.getTime();
-
-      if (!isOpenNow) {
-        const MILLISECONDS_IN_A_DAY = 86400000;
-        const nowIsBeforeOpening = timeNow.getTime() < openingTime.getTime();
-        const nowIsAfterClosing = timeNow.getTime() >= closingTime.getTime();
-
-        if (nowIsBeforeOpening) {
-          timeToChange = openingTime.getTime() - timeNow.getTime();
-        }
-
-        if (nowIsAfterClosing) {
-          timeToChange = openingTime.getTime() + MILLISECONDS_IN_A_DAY;
-        }
-      }
-
-      hoursStatus.push({ isOpenNow, timeToChange });
-    });
-
-    const openHour = hoursStatus.find(hour => hour.isOpenNow);
-
-    if (openHour) return openHour;
-
-    const orderedHourStatus = [...hoursStatus].sort(
-      (a, b) => a.timeToChange - b.timeToChange
-    );
-
-    return orderedHourStatus[0];
+    return getStatusByHours(restaurant.hours);
   }, [restaurant]);
 
   useEffect(() => {
     function changeState() {
-      const { isOpenNow, timeToChange } = getIfIsOpen();
+      const { isActiveNow, timeToChange } = getIfIsOpen();
 
       if (timeToChange != null) {
         setTimeout(changeState, timeToChange);
       }
 
-      setIsOpen(isOpenNow);
+      setIsOpen(isActiveNow);
     }
 
     changeState();
